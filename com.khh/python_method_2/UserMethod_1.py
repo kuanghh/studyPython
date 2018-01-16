d75781b129f9b0049bfd841b33459e7737bf2158
kuanghaohua <kuanghaohua@do1.com.cn>
2018-01-16 11:42:26 +0800

    python  使用函数

---
+++ com.khh/python_method_2/UserMethod_1.py
@@ -0,0 +1,38 @@
+# 使用函数
+
+# Python内置了很多有用的函数，我们可以直接调用。
+# 可以直接从Python的官方网站查看文档：http://docs.python.org/3/library/functions.html
+
+# 也可以在交互式命令行通过help(abs)查看abs函数的帮助信息。
+
+# 调用 abs函数
+print(abs(100))    # 输出 100
+print(abs(-100))   # 输出 100
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
