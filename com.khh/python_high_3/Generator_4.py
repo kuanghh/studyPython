b0a2eb897d95940e5480ada7c01038db63b6c63c
kuanghaohua <kuanghaohua@do1.com.cn>
2018-01-18 09:08:11 +0800

    python  生成器

--- com.khh/python_high_3/Generator_4.py
+++ com.khh/python_high_3/Generator_4.py
@@ -71,7 +71,7 @@ for x in fib_g:
 # 而变成generator的函数，在每次调用next()的时候执行，遇到yield语句返回，再次执行时从上次返回的yield语句处继续执行。
 # 但是用for循环调用generator时，发现拿不到generator的return语句的返回值。如果想要拿到返回值，必须捕获StopIteration错误，返回值包含在StopIteration的value中
 
-
+# 杨辉三角
 def yang(num):
     j = 0
     l = [1]
