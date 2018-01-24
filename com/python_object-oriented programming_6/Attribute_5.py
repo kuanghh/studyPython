53734209cc9ff4c7f84926e6164e82d2e4b26245
kuanghaohua <kuanghaohua@do1.com.cn>
2018-01-24 09:38:34 +0800

    python  实例属性与类属性

---
+++ com/python_object-oriented programming_6/Attribute_5.py
@@ -0,0 +1,29 @@
+# 实例属性 和 类属性
+# 由于Python是动态语言，根据类创建的实例可以任意绑定属性。
+#
+# 给实例绑定属性的方法是通过实例变量，或者通过self变量：
+
+
+class Student(object):
+    score = 10    # 类的属性
+
+    def __init__(self, name):
+        self.name = name        # name是实例的属性
+
+
+# 当我们定义了一个类属性后，这个属性虽然归类所有，但类的所有实例都可以访问到。来测试一下：
+s1 = Student("Mike")
+s2 = Student("Tom")
+
+print(s1.score)  # 输出10
+print(s2.score)  # 输出10
+
+s1.score = 20
+print(s1.score)  # 输出20
+print(s2.score)  # 输出10
+
+
+# 千万不要对实例属性和类属性使用相同的名字，因为相同名称的实例属性将屏蔽掉类属性，但是当你删除实例属性后，再使用相同的名称，访问到的将是类属性。
+
+
+
